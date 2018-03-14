d0ffc29189124b82db2b84668b9cd37f0d72d271
langchristian96 <langchristian96@gmail.com>
2018-03-14 11:16:00 +0200

    add tests

---
+++ src/test/java/ro/ubb/vvss/AppTest.java
@@ -0,0 +1,47 @@
+package ro.ubb.vvss;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import ro.ubb.vvss.controller.MemberController;
+import ro.ubb.vvss.model.Entry;
+import ro.ubb.vvss.repository.MemberRepository;
+
+/**
+ * Created by langchristian96 on 3/14/2018.
+ */
+public class AppTest extends TestCase {
+
+    private MemberRepository repository;
+    private MemberController controller;
+
+    public void test_tc_1_addEntryNegativeId() {
+        repository = new MemberRepository();
+        controller = new MemberController(repository);
+        Entry entry = new Entry("income",123,-1);
+        int size = controller.allEntries().size();
+        controller.addEntry(entry);
+        assertEquals(controller.allEntries().size(), size);
+    }
+
+    public void test_tc_2_addRandomTypeEntry() {
+        repository = new MemberRepository();
+        controller = new MemberController(repository);
+        Entry entry = new Entry("qwe",123,-1);
+        int size = controller.allEntries().size();
+        controller.addEntry(entry);
+        assertEquals(controller.allEntries().size(), size);
+    }
+
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+}
