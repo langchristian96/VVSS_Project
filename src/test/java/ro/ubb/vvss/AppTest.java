cb64c32105898ecf29faf0481ceafde8fa5a08c8
langchristian96 <langchristian96@gmail.com>
2018-03-27 23:27:20 +0300

    break tests

--- src/test/java/ro/ubb/vvss/AppTest.java
+++ src/test/java/ro/ubb/vvss/AppTest.java
@@ -21,7 +21,7 @@ public class AppTest extends TestCase {
         Entry entry = new Entry("income",123,-1);
         int size = controller.allEntries().size();
         controller.addEntry(entry);
-        assertEquals(controller.allEntries().size(), size+1);
+        assertEquals(controller.allEntries().size(), size);
     }
 
     public void test_tc_2_addRandomTypeEntryy() {
@@ -33,7 +33,7 @@ public class AppTest extends TestCase {
         assertEquals(controller.allEntries().size(), size+1);
     }
     public void test_tc_3_empty() {
-        
+
     }
 
     public AppTest( String testName )
